ea76806e9f2559e743a00e7102679ca5b0d9f152
amh6281 <amh6281@naver.com>
2022-08-01 15:04:07 +0900

    feat: JWT Cookie를 통한 로그인 기능 구현 2022.08.01
    JWT Cookie를 사용하여 사용자를 인증하고 로그인 기능을 구현한다.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import { createError } from "../error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
@@ -9,7 +11,29 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ ...req.body, password: hash });
 
     await newUser.save();
-    res.status(200).send("사용자가 생성되었습니다!");
+    res.status(200).send("User has been created!");
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) return next(createError(400, "Wrong Credentials!"));
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT);
+    const { password, ...others } = user._doc;
+
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .json(others);
   } catch (err) {
     next(err);
   }
